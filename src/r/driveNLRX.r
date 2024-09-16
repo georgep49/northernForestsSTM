337400f0c4e7db9888f4499fe25b298f544614ad
georgep49 <gper020@uoa.auckland.ac.nz>
2024-09-16 21:22:53 +1200

    refactoring and fire fixes

--- src/r/driveNLRX.r
+++ src/r/driveNLRX.r
@@ -3,7 +3,8 @@ library(nlrx)
 # This needs NetLogo 6.3.0 or earlier... to do with paths to extensions
 # Windows default NetLogo installation path (adjust to your needs!):
 netlogopath <- file.path("C:/Program Files/NetLogo 6.3.0")
-modelpath <- file.path("H://files.fos/private/Research/NgaRakauTaketake/stm/src/nlogo/stmNorthernForests_63.nlogo")
+# modelpath <- file.path("H://files.fos/private/Research/NgaRakauTaketake/stm/src/nlogo/stmNorthernForests_63.nlogo")
+modelpath <- file.path("D://Research/NgaRakauTaketake/stm/src/nlogo/stmNorthernForests_63.nlogo")
 outpath <- file.path("C:/Temp")
 
 
@@ -26,6 +27,40 @@ nl@experiment <- experiment(expname="test",
                             metrics=c("abundances"),
                             variables = list(),
                             constants = list("fire-frequency" = 0.15))
+
+
+# perc-seed
+# fraction-seed-ldd
+# seed-pred
+# flamm-start
+# track-stalled?
+# mean-ldd
+# base-invasion
+# fire-slow
+# fire-invasion
+# invasion?
+# init-composition-file
+# base-seed-prod-of
+# base-seed-prod-yf
+# crit-density-yng
+# crit-density-old
+# max-ticks
+# burn-in-regen
+# max-forest
+# write-record?
+# extrinsic-sd
+# rust-global-inf
+# phy-global-inf
+# phy-local-inf
+# phy-radius-inf
+# enso-matrix-file
+# sap-herbivory
+# enso-freq-wgt
+# farm-edge?
+# farm-edge-nodes
+# mean-farm-depth
+# farm-revegetate?
+# nlrx-info
 
 # Attach simdesign
 nl@simdesign <- simdesign_simple(nl = nl, nseeds = 3)
